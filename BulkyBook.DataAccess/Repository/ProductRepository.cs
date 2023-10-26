372d146e162cc9e51978bd71ec24903e20c2706e
MislavLesin <pripravnik-mislav.lesin@dump.hr>
2023-10-26 13:56:05 +0200

    Added upsert action methods and view

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    internal class ProductRespository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRespository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product product)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u =>u.Id == product.Id);
+           if(objFromDb != null)
+            {
+                objFromDb.Title = product.Title;
+                objFromDb.ISBN = product.ISBN;
+                objFromDb.ListPrice = product.ListPrice;
+                objFromDb.Price = product.Price;
+                objFromDb.Price50 = product.Price50;
+                objFromDb.Price100 = product.Price100;
+                objFromDb.Description = product.Description;
+                objFromDb.CategoryId = product.CategoryId;
+                objFromDb.Author= product.Author;
+                if(product.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = product.ImageUrl;
+                }
+                
+            }
+        }
+    }
+}
---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+       
+        //GET
+        public IActionResult Upsert(int? Id)
+        {
+            Product product = new();
+            if (Id == null || Id == 0)
+            {
+                return View(product);
+            }
+            else
+            {
+
+            }
+           
+            return View(product);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Successfuly edited Product";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == Id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? Id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Successfuly deleted Product";
+            return RedirectToAction("Index");
+        }
+    }
+}
