67524327ad7db81e75c856d07b2d109ced69c9e4
MislavLesin <pripravnik-mislav.lesin@dump.hr>
2023-11-30 12:33:56 +0100

    Added seed method for deployment

---
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,70 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BulkyBook.Utility;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+            private readonly UserManager<IdentityUser> _userManager;
+            private readonly RoleManager<IdentityRole> _roleManager;
+            private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex) 
+            {
+
+            }
+
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Admin@gmail.com",
+                    Email = "Admin@gmail.com",
+                    Name = "Mislav Lesin",
+                    PhoneNumber = "091091091",
+                    StreetAdress = "Test 33",
+                    State = "Croatia",
+                    PostalCode = "21000",
+                    City = "Split"
+                }, "Admin123*").GetAwaiter().GetResult();
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "Admin@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
+        }
+    }
+}
